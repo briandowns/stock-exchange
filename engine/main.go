e989f8d19475109de21e15fc555f6b64d0f40f02
Brian Downs <brian.downs@gmail.com>
2016-06-18 19:35:17 -0700

    basic engine structur

--- engine/main.go
+++ engine/main.go
@@ -1,15 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 
-	"github.com/briandowns/stock-exchange/database"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/thoas/stats"
+	"github.com/unrolled/render"
 )
 
+const (
+	// APIBase is the base path for API access
+	APIBase = "/api/v1/"
+)
+
+var signalsChan = make(chan os.Signal, 1)
+
 func main() {
-	db, err := database.NewDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	signal.Notify(signalsChan, os.Interrupt)
+
+	go func() {
+		for sig := range signalsChan {
+			log.Printf("Exiting... %v\n", sig)
+			signalsChan = nil
+			os.Exit(1)
+		}
+	}()
+
+	ren := render.New()
+
+	//ob := models.NewOrderBook()
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+	)
+
+	statsMiddleware := stats.New()
+
+	// create the router
+	router := mux.NewRouter()
+
+	// route handler for a health check
+	router.HandleFunc(APIBase+"healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, http.StatusOK)
+	}).Methods("HEAD")
+
+	// route handler for statistics
+	router.HandleFunc(APIBase+"stats", func(w http.ResponseWriter, r *http.Request) {
+		stats := statsMiddleware.Data()
+		ren.JSON(w, http.StatusOK, stats)
+	}).Methods("GET")
+
+	// route handler for the book
+	router.HandleFunc(APIBase+"book", func(w http.ResponseWriter, r *http.Request) {
+		ren.JSON(w, http.StatusOK, "book")
+	}).Methods("GET")
+
+	// route handler for individual book entries
+	router.HandleFunc(APIBase+"book/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		bookID := vars["id"]
+		ren.JSON(w, http.StatusOK, bookID)
+	}).Methods("GET")
+
+	n.Use(statsMiddleware)
+	n.UseHandler(router)
+	n.Run(":7777")
 }
--- models/order_book.go
+++ models/order_book.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Order represents an order
 type Order struct {
 	Time     time.Time
 	Quantity int
@@ -13,4 +14,26 @@ type Order struct {
 // OrderBook represents the current state of the market
 type OrderBook struct {
 	Orders []Order
+}
+
+// Len gets the length of the
+func (o *OrderBook) Len() int {
+	return len(o.Orders)
+}
+
+// Less determins if the first value is before the second
+func (o *OrderBook) Less(i, j int) bool {
+	return o.Orders[i].Time.Before(o.Orders[j].Time)
+}
+
+// Swap will swap the placement of the elements by their indexes
+func (o *OrderBook) Swap(i, j int) {
+	o.Orders[i], o.Orders[j] = o.Orders[j], o.Orders[i]
+}
+
+// NewOrderBook creates a new value of type OrderBook pointer
+func NewOrderBook() *OrderBook {
+	return &OrderBook{
+		Orders: make([]Order, 0),
+	}
 }
