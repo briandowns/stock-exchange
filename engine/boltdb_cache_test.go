ff038aa08ad8c644fa541acd0c0db70bbd2bd168
Brian Downs <brian.downs@gmail.com>
2016-06-26 13:22:35 -0700

    issue-30: beginnings of caching tests

--- engine/boltdb_cache_test.go
+++ engine/boltdb_cache_test.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"log"
 	"testing"
+
+	"github.com/briandowns/stock-exchange/config"
+	"github.com/briandowns/stock-exchange/database"
+
+	"github.com/boltdb/bolt"
 )
+
+var testingDB = "data/testing_db"
+var bConf *config.Config
+var testDB *bolt.DB
+
+// init
+func init() { bSetup() }
+
+// bSetup
+func bSetup() {
+	var err error
+	bConf, err = config.Load(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	testDB, err = database.NewDB(testingDB)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 // TestNewBoltCache
 func TestNewBoltCache(t *testing.T) {
+	NewBoltCache(testDB, bConf)
 }
 
 // TestBuild
 func TestBuildBolt(t *testing.T) {
+	bc := NewBoltCache(testDB, bConf)
+	if err := bc.Build(); err != nil {
+		t.Error(err)
+	}
 }
 
 // TestGet
