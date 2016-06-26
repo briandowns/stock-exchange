8f51c9262e54fdb1310a5e31764610e3101bca8b
Brian Downs <brian.downs@gmail.com>
2016-06-26 12:46:51 -0700

    issue-30: testing skeleton for engine tests

---
+++ engine/redis_cache_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestNewRedisCache
+func TestNewRedisCache(t *testing.T) {
+}
+
+// TestBuild
+func TestBuild(t *testing.T) {
+}
+
+// TestGet
+func TestGet(t *testing.T) {
+}
+
+// TestAdd
+func TestAdd(t *testing.T) {
+}
+
+// TestEntries
+func TestEntries(t *testing.T) {
+}
+
+// TestFlushDB
+func TestFlushDB(t *testing.T) {
+}
